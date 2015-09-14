c1342cfb37f592d03d4bce679468577ae89598bf
Frank Pepermans <frank.pepermans@algomi.com>
2015-09-14 13:51:10 +0100

    no message

--- web/examples/simple.cell.combine.js
+++ web/examples/simple.cell.combine.js
@@ -1,4 +1,3 @@
 Rx.Observable.combineLatest($A1, $B1)
-.bufferWithCount(2, 1)
-.flatMapLatest(x => x)
-.subscribe(x => onvalue(x))
+.bufferWithCount(1)
+.subscribe(x => onvalue(x[0][0] + ' and ' + x[0][1]))
